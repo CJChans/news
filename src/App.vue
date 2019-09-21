f2ce5e784cf4e4a0c9c91fb7144d53d4670b3cb8
CJChans <52377346+CJChans@users.noreply.github.com>
2019-09-21 16:51:38 +0800

    08-项目前准备

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
     <<template>
-        <div class="red">
-            这是第一个单文件组件123，{{message}},
-            <router-link to="/login">登录项</router-link>
-            <router-view></router-view>
+        <div>
+           <div class="box"></div>
         </div>
     </template>
     
@@ -10,15 +8,16 @@
     export default {
         data(){
             return{
-                message:"hello vue"
+                
             }
         }
     }
     </script>
     
     <style>
-        .red{
-            color:green;
-            font-size: 24px;
-        }
+       .box{
+           width: 10vw;
+           height: 10vw;
+           background: #000;
+       }
     </style>
