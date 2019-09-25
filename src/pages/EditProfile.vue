b503c9cac3f5ae26fc14c2a94f33296631cbbcde
CJChans <52377346+CJChans@users.noreply.github.com>
2019-09-25 18:08:44 +0800

    32-编辑昵称的弹窗

--- src/pages/EditProfile.vue
+++ src/pages/EditProfile.vue
@@ -10,7 +10,19 @@
       </div>
 
       <!-- 调用条形组件 -->
-      <CellBar lable="昵称" :text="profile.nickname"/>
+      <CellBar label="昵称" :text="profile.nickname" @click="show1 = !show1"/>
+
+      <!-- 昵称编辑输入框 -->
+      <!-- 鼠标放到属性上就可以查看 -->
+      <van-dialog
+        v-model="show1"
+        title="编辑昵称"
+        show-cancel-button
+        >
+        <!-- value读取昵称 -->
+        <van-field :value="profile.nickname" placeholder="请输入用户名" />
+      </van-dialog>
+
       <CellBar lable="密码" :text="profile.password" type="password"/>
       <CellBar lable="性别" :text="profile.gender"/>
   </div>
@@ -24,7 +36,9 @@ export default {
     data(){
         return{
             //用户详情
-            profile:{}
+            profile:{},
+            //昵称弹窗
+            show1:false,
         }
     },
     components:{
