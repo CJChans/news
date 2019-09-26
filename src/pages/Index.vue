23104d5ba3445f24262196d8e12380e287226ea5
CJChans <52377346+CJChans@users.noreply.github.com>
2019-09-26 20:23:25 +0800

    53-列表分页组件的使用

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -25,7 +25,19 @@
         :key="index"
         :title="item.name"
         >
-            <PostCard v-for="(item,index) in posts" :key="index" :post="item"/>
+           
+            <!-- v-model: 列表是否在加载 -->
+            <!-- finished: 是否加载完毕 -->
+            <!-- load: 到底部触发的事件 -->
+            <van-list
+                v-model="loading"
+                :finished="finished"
+                finished-text="没有更多了"
+                @load="onLoad"
+                >
+
+                 <PostCard v-for="(item,index) in posts" :key="index" :post="item"/>
+            </van-list>
         </van-tab>
     </van-tabs>
 
@@ -47,7 +59,11 @@ export default {
             cid:999,
 
             //默认的文章列表
-            posts:[]
+            posts:[],
+            // 是否在加载,加载完毕后需要手动变为false
+            loading: false,
+            // 是否有更多数据，如果加载完所有的数据，改为true
+            finished: false
         }
     },
 
@@ -61,6 +77,17 @@ export default {
 
     components:{
         PostCard
+    },
+
+     methods: {
+        // 加载下一页的数据
+        onLoad(){
+            setTimeout(() => {
+                console.log("已经滚动到底部");
+                this.loading = false;
+                this.finished = true;
+            }, 2000)
+        }
     },
 
     mounted(){
